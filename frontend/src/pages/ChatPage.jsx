153facd12a186018e6280312cf3fd6fbfd53a30d
Victor <victor.dmaina@gmail.com>
2025-02-19 17:46:36 +0300

    improve responsivennes

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,60 +1,3 @@
-// // pages/ChatPage.jsx
-// // eslint-disable-next-line no-unused-vars
-// import React, { useState } from 'react';
-// import ChatMessage from '../components/ChatMessage';
-
-// function ChatPage() {
-//   const [messages, setMessages] = useState([]);
-//   const [input, setInput] = useState('');
-
-//   const handleSend = () => {
-//     if (input.trim()) {
-//       setMessages([...messages, { content: input, isBot: false }]);
-//       // Add chatbot response logic here
-//       setInput('');
-//     }
-//   };
-
-//   const handleKeyDown = (e) => {
-//     if (e.key === 'Enter') {
-//       handleSend();
-//     }
-//   };
-
-//   return (
-//     <div className="h-full flex flex-col">
-//       <div className="flex-1 overflow-y-auto mb-4 space-y-4">
-//         {messages.map((msg, index) => (
-//           <ChatMessage
-//             key={index}
-//             message={{ type: 'text', content: msg.content }}
-//             isBot={msg.isBot}
-//           />
-//         ))}
-//       </div>
-
-//       <div className="flex gap-2">
-//         <input
-//           type="text"
-//           value={input}
-//           onChange={(e) => setInput(e.target.value)}
-//           onKeyDown={handleKeyDown}
-//           className="flex-1 p-3 border border-[#62767c] rounded-lg text-gray-400"
-//           placeholder="Type your message..."
-//         />
-//         <button
-//           onClick={handleSend}
-//           className="px-6 py-3 bg-[#62767c] text-white rounded-lg hover:bg-[#90a9b1]"
-//         >
-//           Send
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ChatPage;
-
 // pages/ChatPage.jsx
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useRef, useEffect } from "react";
@@ -171,7 +114,7 @@ function ChatPage() {
         {/* Send Button */}
         <button
           onClick={handleSend}
-          className="px-6 py-2 bg-[#62767c] rounded-lg hover:bg-[#90a9b1] w-8 sm:w-full text-m sm:text-2xl "
+          className="p-2 bg-[#62767c] rounded-lg hover:bg-[#90a9b1] w-8 sm:w-16 text-xs sm:text-xl "
         >
           Send
         </button>
